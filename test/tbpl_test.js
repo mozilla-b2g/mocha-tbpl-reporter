2e199bf3de8bcf60d8faee3dbdb5bc396ed61648
Gareth Aye <gaye@mozilla.com>
2014-01-14 17:03:43 -0500

    Bug 959816 - Improve mocha-tbpl-reporter error reporting

--- test/tbpl_test.js
+++ test/tbpl_test.js
@@ -1,112 +1,100 @@
-
 var TBPL = require('../lib/tbpl'),
     assert = require('assert'),
     sinon = require('sinon');
 
 suite('TBPL', function() {
-  var runner, spy, subject;
+  var runner, on, log;
 
   setup(function() {
     runner = {
       on: function(type, listener) {}
     };
 
-    spy = sinon.spy(runner, 'on');
+    on = sinon.spy(runner, 'on');
+    log = sinon.spy(console, 'log');
     subject = new TBPL(runner);
   });
 
+  teardown(function() {
+    on.restore();
+    log.restore();
+  });
+
   test('#constructor', function() {
-    sinon.assert.calledWith(spy, 'end');
-    sinon.assert.calledWith(spy, 'fail');
-    sinon.assert.calledWith(spy, 'pass');
-    sinon.assert.calledWith(spy, 'pending');
-    sinon.assert.calledWith(spy, 'test');
-    sinon.assert.calledWith(spy, 'test end');
+    sinon.assert.calledWith(on, 'end');
+    sinon.assert.calledWith(on, 'fail');
+    sinon.assert.calledWith(on, 'pass');
+    sinon.assert.calledWith(on, 'pending');
+    sinon.assert.calledWith(on, 'test');
+    sinon.assert.calledWith(on, 'test end');
     assert.strictEqual(subject.failing, 0);
     assert.strictEqual(subject.passing, 0);
     assert.strictEqual(subject.pending, 0);
   });
 
   test('#onEnd', function() {
-    var stub = sinon.stub(console, 'log');
     subject.onEnd();
-    console.log.restore();
-
-    assert.ok(stub.calledWith('*~*~* Results *~*~*'));
-    assert.ok(stub.calledWith('passed: %d', 0));
-    assert.ok(stub.calledWith('failed: %d', 0));
-    assert.ok(stub.calledWith('todo: %d', 0));
+    assert.ok(log.calledWith('*~*~* Results *~*~*'));
+    assert.ok(log.calledWith('passed: %d', 0));
+    assert.ok(log.calledWith('failed: %d', 0));
+    assert.ok(log.calledWith('todo: %d', 0));
   });
 
   test('#onFail', function() {
     var failing = subject.failing;
-    var stub = sinon.stub(console, 'log');
     subject.onFail({
       fullTitle: function() {
         return 'some title';
       }
-    });
-    console.log.restore();
+    }, new Error('ruhroh'));
 
     assert.strictEqual(subject.failing, failing + 1);
-    assert.ok(
-        stub.calledWith('TEST-UNEXPECTED-FAIL | %s', 'some title'));
+    assert.ok(log.calledWith('TEST-UNEXPECTED-FAIL | %s', 'some title'));
+    assert.ok(log.calledWith('ruhroh'));
   });
 
   test('#onPass', function() {
     var passing = subject.passing;
-    var stub = sinon.stub(console, 'log');
     subject.onPass({
       fullTitle: function() {
         return 'some title';
       }
     });
-    console.log.restore();
 
     assert.strictEqual(subject.passing, passing + 1);
-    assert.ok(
-        stub.calledWith('TEST-PASS | %s', 'some title'));
+    assert.ok(log.calledWith('TEST-PASS | %s', 'some title'));
   });
 
   test('#onPending', function() {
     var pending = subject.pending;
-    var stub = sinon.stub(console, 'log');
     subject.onPending({
       fullTitle: function() {
         return 'some title';
       }
     });
-    console.log.restore();
 
     assert.strictEqual(subject.pending, pending + 1);
-    assert.ok(
-        stub.calledWith('TEST-PENDING | %s', 'some title'));
+    assert.ok(log.calledWith('TEST-PENDING | %s', 'some title'));
   });
 
   test('#onTest', function() {
-    var stub = sinon.stub(console, 'log');
     subject.onTest({
       fullTitle: function() {
         return 'some title';
       }
     });
-    console.log.restore();
 
-    assert.ok(
-        stub.calledWith('TEST-START | %s', 'some title'));
+    assert.ok(log.calledWith('TEST-START | %s', 'some title'));
   });
 
   test('#onTestEnd', function() {
-    var stub = sinon.stub(console, 'log');
     subject.onTestEnd({
       fullTitle: function() {
         return 'some title';
       }
     });
-    console.log.restore();
 
-    assert.ok(
-        stub.calledWith('TEST-END | %s', 'some title'));
+    assert.ok(log.calledWith('TEST-END | %s', 'some title'));
   });
 
   test('#getTitle', function() {
